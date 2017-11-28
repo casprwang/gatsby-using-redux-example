8a84f15315db9073ea0e4da0cede06517d00ba7b
wangsongiam <wangsongiam@gmail.com>
2017-11-27 20:17:15 -0500

    update

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/song/dev/using-redux/src/layouts/index.js"
-  import data from "/Users/song/dev/using-redux/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
