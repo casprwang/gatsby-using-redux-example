c56fa7c0101803e5dc34c5e1d154bd16f1beca6b
wangsongiam <wangsongiam@gmail.com>
2017-11-27 20:16:02 -0500

    🎉 rebase

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/song/dev/using-redux/src/layouts/index.js"
+  import data from "/Users/song/dev/using-redux/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
---
+++ src/state/createStore.js
@@ -0,0 +1,16 @@
+import { combineReducers, createStore as reduxCreateStore } from "redux"
+
+const initialState = { count: 0 }
+
+const reducer = (state={}, action) => {
+  if (action.type === `INCREMENT`) {
+    return Object.assign({}, state, {
+      count: state.count + 1,
+    })
+  }
+  return state
+}
+
+
+const createStore = () => reduxCreateStore(combineReducers({reducer}), initialState)
+export default createStore
